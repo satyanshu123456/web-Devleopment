99d8e3227e064c30c7f7b4f8eeed08bb7178848e
satyanshu123456 <satyanshugautam6@gmail.com>
2026-04-20 20:44:58 +0530

    Add initial HTML and JavaScript files with basic structure and console logs

---
+++ jBasic/script.js
@@ -0,0 +1,22 @@
+console.log("hello world");
+
+let a=5;
+console.log(a);
+var x=5;
+var x=6;
+console.log(x);
+
+const pi=3.14;
+console.log(pi);
+
+let c="satya";
+
+
+let p=10;
+let q=20;
+console.log(p==q);
+console.log(p===q);
+
+
+console.log(typeof p);
+console.log(typeof c);
