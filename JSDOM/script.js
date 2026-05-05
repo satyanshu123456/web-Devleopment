8caa469764d6e55c1377cd95cfc9c89cdd9992a5
satyanshu123456 <satyanshugautam6@gmail.com>
2026-05-05 18:55:05 +0530

    Enhance form functionality in index.html and script.js; add email, city, and password fields with corresponding data display

--- JSDOM/script.js
+++ JSDOM/script.js
@@ -1,11 +1,22 @@
 function Submit() {
   console.log("Submit Button Clicked");
   const fn = document.getElementById("FullName").value;
+  const fn1 = document.getElementById("EmailAddress").value;
+  const fn2 = document.getElementById("city").value;
+  const fn3 = document.getElementById("UserPass").value;
   console.log(fn);
+  console.log(fn1);
+  console.log(fn2);
+  console.log(fn3);
 
   document.getElementById("myData").innerText = fn;
-  document.getElementById("myData2").innerText = fn;
+  document.getElementById("myData2").innerText = fn1;
+  document.getElementById("myData3").innerText = fn2;
+  document.getElementById("myData4").innerText = fn3;
   document.getElementById("FullName").value = "";
+  document.getElementById("EmailAddress").value = "";
+  document.getElementById("city").value = "";
+  document.getElementById("UserPass").value = "";
 
   document.getElementById("dataCard").classList.add("divshow");
   document.getElementById("dataCard").classList.remove("divsHide");
