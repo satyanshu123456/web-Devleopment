550d273744b001d27aee8847421633ea514c124f
satyanshu123456 <satyanshugautam6@gmail.com>
2026-05-07 19:31:05 +0530

    Add initial HTML, CSS, and JavaScript files for bulb color change functionality

---
+++ JSEventHandling/script.js
@@ -0,0 +1,19 @@
+function Bulb1ON(){
+    document.getElementById("bulb1").style.backgroundColor = "yellow";
+}
+
+function Bulb1OFF(){
+    document.getElementById("bulb1").style.backgroundColor = "white";
+}
+
+function Bulb1RED(){
+    document.getElementById("bulb1").style.backgroundColor = "red";
+}
+
+function Bulb1GREEN(){
+    document.getElementById("bulb1").style.backgroundColor = "green";
+}
+
+function Bulb1BLUE(){
+    document.getElementById("bulb1").style.backgroundColor = "blue";
+}
