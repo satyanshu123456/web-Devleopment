da8d049591403746065839836b873ad826294a51
satyanshu123456 <satyanshugautam6@gmail.com>
2026-05-10 15:34:06 +0530

    Update form fields to include password input and enhance event handling for form submission

--- JSEventHandlingAdvance/script1.js
+++ JSEventHandlingAdvance/script1.js
@@ -1,1 +1,25 @@
-// document.querySelector("form").addEventListener("submit", event.preventDefault();)
+document.querySelector("form").addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const fn = document.getElementById("FullName").value;
+  const em = document.getElementById("email").value;
+  const ps = document.getElementById("password").value;
+
+  const dataPacket = {
+    FullName: fn,
+    Email: em,
+    Password: ps,
+  };
+
+  console.log(dataPacket);
+
+  document.getElementById("FullName").value = "";
+  document.getElementById("email").value = "";
+  document.getElementById("password").value = "";
+});
+
+document.querySelector("form").addEventListener("reset", (event) => {
+  event.preventDefault();
+
+  confirm("Are You Sure") && window.Location.reload();
+});
--- Projects/Age_Calculator/script.js
+++ Projects/Age_Calculator/script.js
@@ -3,6 +3,8 @@ document.querySelector("form").addEventListener("submit",(event)=>{
 
     const dob =document.getElementById("DOB").value;
     const curDate = document.getElementById("CurrentDate").vcalue;
+    console.log(dob);
+    console.log(CurDate);
 
     const Age = Number(curDate.split("-"[0])) - Number(dob.split("-")[0]);
 
