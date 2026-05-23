e0c5907f7901d58fd647348fbfe4215e8d1bb49e
satyanshu123456 <satyanshugautam6@gmail.com>
2026-05-23 20:24:13 +0530

    Add initial HTML, CSS, and JavaScript files for JokeApp

---
+++ Projects/JokeApp/script.js
@@ -0,0 +1,10 @@
+async function GetNewJoke() {
+  const API_URL = "https://v2.jokeapi.dev/joke/Programming,Miscellaneous";
+
+  const response = await fetch(API_URL);
+
+  const data = await response.json();
+
+  document.getElementById("setup").innerText = data.joke || data.setup;
+  document.getElementById("delivery").innerText = data.delivery || "";
+}
