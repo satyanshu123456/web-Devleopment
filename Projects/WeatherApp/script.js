63a393d5ed6ecfab1949f73a4cfc9c741f757bc3
satyanshu123456 <satyanshugautam6@gmail.com>
2026-05-24 13:18:40 +0530

    Refactor code structure for improved readability and maintainability

--- Projects/WeatherApp/script.js
+++ Projects/WeatherApp/script.js
@@ -2,21 +2,33 @@ const API_KEY = "44853336a00a3932b1ca67039556ec33";
 
 async function getWeather() {
   const cityName = document.getElementById("cityName").value.trim();
+
   const { Lattitude, Longitude } = await getGeoLocation(cityName);
 
-  console.log({ Lattitude, Longitude });  
+  //   console.log({ Lattitude, Longitude });
+
+  const WEATHER_API = `https://api.openweathermap.org/data/2.5/weather?lat=${Lattitude}&lon=${Longitude}&appid=${API_KEY}`;
+
+  const response = await fetch(WEATHER_API);
+  const data = await response.json();
+
+  //console.log(data);
+
+  const temperature = data.main.temp - 273.15;
+
+  document.getElementById("Temperature").innerText = temperature.toFixed(2);
 }
 
 async function getGeoLocation(city) {
-  const GEO_LOC_API = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${API_key}`;
+  const GEO_LOC_API = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${API_KEY}`;
 
-  const respose = await fetch(GEO_LOC_API);
-  const data = await Response.json();
+  const response = await fetch(GEO_LOC_API);
+  const data = await response.json();
 
-  conmsole.log(data);
+  //   console.log(data);
 
   const Lattitude = data[0].lat;
-  const Longitude = data[0].lat;
+  const Longitude = data[0].lon;
 
   return { Lattitude, Longitude };
 }
